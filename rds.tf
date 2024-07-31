c8f2dcc0fb5bc61929726390fa1a1b966c50fb1c
pavantocemca40 <pavan.tocemca40@gmail.com>
2024-07-31 16:57:02 +0530

    updated dbs ubnet group details

--- rds.tf
+++ rds.tf
@@ -1,14 +1,14 @@
 # Creating RDS Instance
-resource "aws_db_subnet_group" "db-group" {
+resource "aws_db_subnet_group" "my-database-group" {
   name       = "main"
   subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-2.id]
   tags = {
     Name = "My DB subnet group"
   }
 }
-resource "aws_db_instance" "db-group" {
+resource "aws_db_instance" "my-database-group" {
   allocated_storage    = 10
-  db_subnet_group_name = aws_db_subnet_group.db-group.id
+  db_subnet_group_name = aws_db_subnet_group.my-database-group.id
   engine               = "mysql"
   engine_version       = "8.0.35"
   instance_class       = "db.t2.micro"
